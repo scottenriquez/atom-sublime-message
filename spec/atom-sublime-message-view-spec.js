6a0d2a27d007f4b20baa65a7f605da963ed2ecb3
Scott Enriquez <scottie@enriquez.io>
2016-08-05 18:25:02 -0500

    Initially generated package

---
+++ spec/atom-sublime-message-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSublimeMessageView from '../lib/atom-sublime-message-view';
+
+describe('AtomSublimeMessageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
